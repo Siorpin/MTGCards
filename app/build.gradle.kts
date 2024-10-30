4f46f079f9306807e990773fc4880cd6afb4568c
Kuba Sierpień <kubasierpien05@gmail.com>
2024-10-30 22:43:33 +0100

    new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.retrofitGson)
+    implementation(libs.coil)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
